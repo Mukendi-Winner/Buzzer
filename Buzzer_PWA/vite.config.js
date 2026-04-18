71eeb7b8216e8bc401dc93169620541b682dbe26
Mukendi Winner <mukendiwinner83@gmail.com>
2026-04-18 14:51:32 +0200

    removing maskable icon so the logo can render correctly on android

--- Buzzer_PWA/vite.config.js
+++ Buzzer_PWA/vite.config.js
@@ -28,13 +28,13 @@ export default defineConfig({
             type: 'image/png',
           },
           {
-            src: '/pwa/icon-192-maskable.png',
+            src: '/pwa/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable',
           },
           {
-            src: '/pwa/icon-512-maskable.png',
+            src: '/pwa/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
